cda8ad4373b262018e7049d611d4a79e67ad4a22
claster Pack <clasterpack@gmail.com>
2022-05-12 21:42:20 +0700

    Сделал кастомную форму регистрации.

--- news_site/accounts/views.py
+++ news_site/accounts/views.py
@@ -1,6 +1,8 @@
 from django.contrib.auth import login, logout
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
 from django.shortcuts import render, redirect
+
+from .forms import RegistrationForm
 
 
 def login_view(request):
@@ -23,9 +25,9 @@ def logout_view(request):
 
 
 def user_registration(request):
-    user_register_form = UserCreationForm()
+    user_register_form = RegistrationForm()
     if request.method == 'POST':
-        user_register_form: UserCreationForm = UserCreationForm(request.POST, files=request.FILES)
+        user_register_form: RegistrationForm = RegistrationForm(request.POST, files=request.FILES)
         if user_register_form.is_valid():
             accounts: user_register_form.save(commit=False)
             accounts.save()
@@ -38,3 +40,12 @@ def user_registration(request):
             'form': user_register_form,
         }
     )
+
+
+def register_view(request):
+    form = RegistrationForm(request.POST or None)
+    if form.is_valid():
+        user_obj = form.save()
+        return redirect('/login')
+    context = {"form": form}
+    return render(request, "accounts/register.html", context)
--- news_site/news/forms.py
+++ news_site/news/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 
+from .models import Post
+
 
 class PostForm(forms.Form):
     title = forms.CharField(max_length=250)
@@ -7,4 +9,16 @@ class PostForm(forms.Form):
     content = forms.CharField()
     status = forms.ChoiceField()
 
+
 class NewPostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'slug', 'content', 'status']
+
+    def clean(self):
+        data = self.clean_data
+        title = data.get("title")
+        qs = Post.objects.filter(title__icontains=title)
+        if qs.exists():
+            self.add_error("Оглавление", f"\"{title}\" уже используеться/")
+        return data
